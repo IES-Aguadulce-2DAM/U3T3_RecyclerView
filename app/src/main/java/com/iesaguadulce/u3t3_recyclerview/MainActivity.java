f940ee1fa70536159825079da335774ca85271ee
José A. González <champunes@tutanota.com>
2023-12-05 16:55:03 +0100

    Plantilla de proyecto
    UD03 Tarea 3

---
+++ app/src/main/java/com/iesaguadulce/u3t3_recyclerview/MainActivity.java
@@ -0,0 +1,13 @@
+package com.iesaguadulce.u3t3_recyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
